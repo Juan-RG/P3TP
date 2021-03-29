9f17386f11f3f70ccbda1fb038a4eb6e3199a9a4
Juan <juanico11@hotmail.com>
2021-03-29 14:57:28 +0200

    Codigo okey

--- Camion.h
+++ Camion.h
@@ -15,7 +15,7 @@ private:
 
 public:
     //Camion(double capacidad) : Almacen<T>(capacidad){}; //todo: Preguntar
-    Camion(double capacidad) : Almacen<Carga>(capacidad){};
+    Camion(double capacidad) : nombre("Camion"), Almacen<Carga>(capacidad){};
 
     bool guardar(Carga& elemento){
         if(this->capacidad >= elemento.getVolumen()){     //Si tiene espacio en el contenedor
@@ -41,7 +41,20 @@ public:
         return volumen;
     }
 
+    friend std::ostream& operator<<(std::ostream& os, const Camion& dt);
+
 };
 
+std::ostream& operator<<(std::ostream& os, const Camion& ca)
+{
+    os << ca.nombre + " [" + std::to_string(ca.capacidad)
+                        + " m3] [" + std::to_string(ca.peso ) + " kg] \n";
+
+
+    for(Carga* a :  ca.elementos){  //Devuelve el toString de cada uno de los elementos guardados
+        os << "   " + a->to_string() + "\n"; //TODO: devuelve el toString de carga
+    }
+    return os;
+}
 
 
